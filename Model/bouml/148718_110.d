5856681cc585c46970c885f98253d7c65e50a227
Jia Qingtong <wanywhn@qq.com>
2018-06-03 14:39:32 +0800

    又实现了几个用例

--- Model/bouml/148718_110.d
+++
@@ -1,7 +0,0 @@
-format 218
-
-classcanvas 128110 class_ref 148590 // BankBoundry
-  classdiagramsettings member_max_width 0 end
-  xyz 176 59 2000
-end
-end
--- Model/bouml/148846_110.d
+++
@@ -1,7 +0,0 @@
-format 218
-
-classcanvas 128110 class_ref 148718 // open_one_card_control
-  classdiagramsettings member_max_width 0 end
-  xyz 136 104 2000
-end
-end
